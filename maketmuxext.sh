026fd156dbf133be7f3bc817930e3cf9de90299c
Takeshi SHIMADA <tk.shimada.0912@gmail.com>
2017-05-19 09:47:25 +0800

    update maketmuxext.sh

--- maketmuxext.sh
+++ maketmuxext.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 TMUXEXT=${HOME}/.tmux.ext.conf
-touch ${TMUXEXT}
+rm -f ${TMUXEXT} && touch ${TMUXEXT}
 
 # powerline
-PLUDIRS=(${HOME}/Library/Python/2.{1..9}/lib/python/site-packages ${HOME}/.local/lib/python2.{1..9}/site-packages)
-PLSDIRS=(/usr/local/lib/python2.{1..9}/{site,dist}-packages)
+PLUDIRS=$(echo ${HOME}/Library/Python/2.{1..9}/lib/python/site-packages ${HOME}/.local/lib/python2.{1..9}/site-packages)
+PLSDIRS=$(echo /usr/local/lib/python2.{1..9}/{site,dist}-packages)
 PLCONF="powerline/bindings/tmux/powerline.conf"
 for DIR in ${PLUDIRS} ${PLSDIRS}; do
         if [ -f ${DIR}/${PLCONF} ]; then
-		echo ${DIR}/${PLCONF}
                 echo source \"${DIR}/${PLCONF}\" >> ${TMUXEXT}
         fi
 done
